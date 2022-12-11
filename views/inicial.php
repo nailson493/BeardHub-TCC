0d63ca94b2c99ed8b583eec6d76c56a988b6f9d0
Kaique Costa Mendes <kaique.mendesdev@gmail.com>
2022-12-11 18:23:58 -0300

    Docs Firebase

--- views/inicial.php
+++ views/inicial.php
@@ -3,7 +3,30 @@
 <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
 <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script> -->
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDI3VuHBMyP9ypp686OqbRR38NQzBjIaIE",
+    authDomain: "beardhub-bddd9.firebaseapp.com",
+    databaseURL: "https://beardhub-bddd9-default-rtdb.firebaseio.com",
+    projectId: "beardhub-bddd9",
+    storageBucket: "beardhub-bddd9.appspot.com",
+    messagingSenderId: "964152416726",
+    appId: "1:964152416726:web:77d6726a9b718919a16c77",
+    measurementId: "G-XFP1756P75"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 <div id="container">
   <div class="liss"> 
