ca7f8f429425e2ae6a2eb70647d4078634d6ec28
nailson493 <84484781+nailson493@users.noreply.github.com>
2022-12-20 11:26:52 -0300

    ATT, ESTABELECIMENTO

---
+++ .history/painel_20221220100626.php
@@ -0,0 +1,45 @@
+<?php
+ include "head.php";
+?>
+
+
+
+<body>
+    <?php
+    include "header.php";
+
+
+
+    switch($_GET['r']){
+
+        case 'signup':
+                include "views/signup.php";
+                break;
+        case 'login':
+                include "views/login.php";
+                break;
+        case 'inicial':
+                include "views/inicial.php";
+                break;
+        case 'cadUser':
+                include "views/cadUser.php";
+                break;
+        case 'estabelecimento':
+                include "views/estabeleciment0.php";
+                break;
+        default:
+                include "views/inicial.php";
+                break;
+            
+    }
+    
+
+
+    ?>
+    <?php include "footer.php";?>
+    
+ <?php include "js.php"; ?>
+ 
+
+</body>
+</html>
---
+++ .history/test_20221220101130.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=0.55">
+    <!-- <meta name="viewport" content= "width=device-width, user-scalable=no"> -->
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" /> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+    <link rel="stylesheet" href="/_css/estabelecimento.css">
+    <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+    <script src="https://unpkg.com/flowbite@1.5.5/dist/datepicker.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+
+</head>
